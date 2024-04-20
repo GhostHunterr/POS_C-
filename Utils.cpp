49b48b1ffd5660bfb12296a2be455977b0ffac9a
Farhan <farhansarang7@gmail.com>
2024-04-20 16:43:56 -0400

    Added Method: clearInputBuffer().

---
+++ Utils.cpp
@@ -0,0 +1,20 @@
+#include "Utils.h"
+#include <iostream>
+#include <limits>
+
+using namespace std;
+using namespace sdds;
+
+
+namespace sdds
+{
+	//Clears input buffer.
+	void clearInputBuffer()
+	{
+		//Clears the error state.
+		cin.clear();
+
+		//Discards the input in input buffer.
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+	}
+}
